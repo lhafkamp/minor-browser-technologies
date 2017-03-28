d7561cddddbd721e3c83c228264a3a0451eec8f3
Luuk Hafkamp <luukhafkamp@gmail.com>
2017-03-28 15:51:27 +0200

    Feature/node (#2)
    * set up server
    
    * get/post works
    
    * item in lists

---
+++ week 3/server.js
@@ -0,0 +1,37 @@
+// require modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+// get the public files
+app.use(express.static('public'));
+
+// ejs
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// use body parser and make it json
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const data = [];
+
+// post form data
+app.post('/', (req, res) => {
+	data.push(req.body.ingredients);
+	res.redirect('/');
+})
+
+// get form data
+app.get('/', (req, res) => {
+	res.render('index', {
+		data: data
+	});
+});
+
+// run app on 9000
+app.listen(3001, () => {
+	console.log('running on 3001...');
+});
