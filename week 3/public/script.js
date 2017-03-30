b88a4b50b857a928f0a166d471da7b1d03771d15
lhafkamp <Luuk@164.153.028.145.hva.nl>
2017-03-30 17:07:52 +0200

    css updates

--- week 3/public/script.js
+++ week 3/public/script.js
@@ -1,6 +1,15 @@
-const box = document.querySelectorAll('.list');
-const images = document.querySelectorAll('img');
-const inputs = document.querySelectorAll('input');
+document.querySelector('form').addEventListener('submit', function(ev) {
+	// Use 'value=".." name="ingredients"'
+	// Submit form with name=value pair, using 'Ajax'
+	// Parse response body
+	// Overwrite list with response body list
+	// Flash the list
+	// ev.preventDefault();
+})
+
+var box = document.querySelectorAll('.list');
+var images = document.querySelectorAll('img');
+var inputs = document.querySelectorAll('input');
 
 function drag(e) {
 	e.dataTransfer.setData('id', e.target.id);
@@ -19,30 +28,36 @@ function dontDrop(e) {
 function drop(e) {
 	e.preventDefault();
 	console.log(e.target);
-	const id = e.dataTransfer.getData('id');
+	var id = e.dataTransfer.getData('id');
 	inputs[id].click();
 	this.classList.remove('floating');
 	this.classList.add('succes');
 }
 
-document.querySelector('form').addEventListener('submit', function(ev) {
-	// Use 'value=".." name="ingredients"'
-	// Submit form with name=value pair, using 'Ajax'
-	// Parse response body
-	// Overwrite list with response body list
-	// Flash the list
-	// ev.preventDefault();
-})
-
 function dragSucces(e) {
-	box.forEach(box => box.classList.add('succes'));
+	box.forEach(function(box) {
+		return box.classList.add('succes');
+	});
 }
 
 function dragFail(e) {
-	box.forEach(box => box.classList.remove('succes'));
+	box.forEach(function (box) {
+		return box.classList.remove('succes');
+	});
 }
 
-images.forEach(img => img.addEventListener('dragstart', drag));
-box.forEach(b => b.addEventListener('dragover', allowDrop));
-box.forEach(b => b.addEventListener('dragleave', dontDrop));
-box.forEach(b => b.addEventListener('drop', drop));
+images.forEach(function (img) {
+  return img.addEventListener('dragstart', drag);
+});
+
+box.forEach(function (b) {
+  return b.addEventListener('dragover', allowDrop);
+});
+
+box.forEach(function (b) {
+  return b.addEventListener('dragleave', dontDrop);
+});
+
+box.forEach(function (b) {
+  return b.addEventListener('drop', drop);
+});
